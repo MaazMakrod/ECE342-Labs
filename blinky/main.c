e1de22814c6a950e36d12883d3e2fc3b278228ec
Maaz Makrod <67477587+MaazMakrod@users.noreply.github.com>
2024-08-04 21:26:55 -0400

    Create main.c

---
+++ blinky/main.c
@@ -0,0 +1,56 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#include "main.h"
+#include <stdio.h>
+#include "config.h"
+
+int main(void)
+{
+  /* Reset of all peripherals. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  
+  char message[100];
+  sprintf(message, "Printing test");
+  print_msg(message);
+	
+	int flip = 0;
+
+  /* Infinite loop */
+  while (1)
+  {
+		
+		if(flip) {
+			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+			HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);	
+			HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);	
+		} else
+			HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		
+		flip = 1;
+    HAL_Delay(500);
+  }
+}
